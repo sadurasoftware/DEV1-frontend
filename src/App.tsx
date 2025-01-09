ad83a295670ce8127d526d3abfb613beffb2f50e
Deepika140101 <deepika@sadurasoftware.com>
2025-01-09 13:38:16 +0530

    first_commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className="flex justify-center">Dev Frontend</h1>
+      <h1 className="flex justify-center">Deepika Dev Frontend</h1>
     </>
   );
 }
