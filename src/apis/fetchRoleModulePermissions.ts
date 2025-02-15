088e83b4398e8f469136f0ca5c875049fcf5c789
vigneshwari <vigneshwari@sadurasoftware.com>
2025-02-15 13:14:12 +0530

    fetch rolemodulepermission Api

--- src/apis/fetchRoleModulePermissions.ts
+++ src/apis/fetchRoleModulePermissions.ts
@@ -26,3 +26,32 @@ export const RoleModulePermissionFetch = async (
     throw new Error('Failed to create role module permissions')
   }
 }
+
+// import { RoleModulePermission } from '@/types/roleModulePermissionType'
+// import axios, { AxiosResponse } from 'axios'
+// import { useLoginInfoStore } from '../store/useLoginInfoStore'
+
+// export const RoleModulePermissionFetch = async (
+//   roleId: number
+// ): Promise<RoleModulePermission> => {
+//   const token = useLoginInfoStore.getState().token
+//   try {
+//     const res: AxiosResponse<RoleModulePermission> = await axios.get(
+//       `http://localhost:3000/api/role-module-permissions/modulespermissionsByRole`,
+//       {
+//         params: { roleId },
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       }
+//     )
+
+//     if (res.status !== 200) {
+//       throw new Error('Error fetching API: ' + res.status)
+//     }
+//     return res.data
+//   } catch (error) {
+//     console.error('Error fetching role module permissions:', error)
+//     throw new Error('Failed to fetch role module permissions')
+//   }
+// }
