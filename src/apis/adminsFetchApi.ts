8fc5a6340bf5e083763fca3cf4cae5c7ce072431
Deepika140101 <deepika@sadurasoftware.com>
2025-04-28 17:48:27 +0530

    Ticket history added

--- src/apis/adminsFetchApi.ts
+++ src/apis/adminsFetchApi.ts
@@ -1,10 +1,17 @@
 import api from '@/lib/api'
 import { adminsResponse } from '@/types/registerTypes'
+import { useLoginInfoStore } from '@/store/useLoginInfoStore'
 
 export const fetchAdmins = async (page:number, search:string, departmentName:string): Promise<adminsResponse> => {
+  const token = useLoginInfoStore.getState().token
   try {
     const response = await api.get(
-      `/api/user/admins?page=${page}&search=${search}&departmentName=${departmentName}`
+      `/api/user/admins?page=${page}&search=${search}&departmentName=${departmentName}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     )
     return response.data
   } catch (error) {
